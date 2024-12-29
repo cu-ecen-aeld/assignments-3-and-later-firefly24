c0797e10832019a81f36014a1eb2ecb949080893
firefly24 <hotkar.darshana@gmail.com>
2024-12-30 03:49:00 +0530

    add_impl

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,13 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+	bool ret= false;
+	ret = system(cmd);
+	
+	if (ret == 0)
+		return true;
+	else
+		return false;
 }
 
 /**
@@ -39,7 +44,8 @@ bool do_exec(int count, ...)
     va_list args;
     va_start(args, count);
     char * command[count+1];
-    int i;
+    int i,ch_status;
+    pid_t ch_pid;
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
@@ -48,7 +54,27 @@ bool do_exec(int count, ...)
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
     command[count] = command[count];
-
+    
+    ch_pid = fork();
+    
+    if (ch_pid < 0 )
+    	return false;
+    	
+    if (ch_pid == 0 ) {
+    	//printf("child process***************\n");
+    	execv(command[0],command);
+    	//printf("return value: %d\n", ret);
+    	exit(1);
+    	
+    }
+    else {
+    	pid_t waitch_pid = wait(&ch_status);
+    	if (waitch_pid < 0)
+    	    return false;
+    	if (WEXITSTATUS(ch_status) == EXIT_FAILURE) 
+    		return false;
+    } 
+    	
 /*
  * TODO:
  *   Execute a system command by calling fork, execv(),
@@ -58,7 +84,7 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
-
+	
     va_end(args);
 
     return true;
@@ -74,18 +100,52 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     va_list args;
     va_start(args, count);
     char * command[count+1];
-    int i;
+    int i, redir_fd, ch_pid, ch_status=0;
+    printf("OP  file : %s", outputfile);
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
+        printf("* %s ", command[i] );
     }
+
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+    
+    redir_fd = open (outputfile,O_CREAT|O_RDWR,644 ); //open output file
+    if (redir_fd <0)
+    	return false;
+    
+    ch_pid = fork();
+    
+    if (ch_pid < 0 )
+    	return false;
+    	
+    if (ch_pid == 0 ) {
+    	//printf("child process***************\n");
+    	
+    	if (dup2(redir_fd, 1) < 0){
+ 	  //redirect stdout to output file 
+ 	  close(redir_fd);
+    		exit(1);
+    	}
+    	execv(command[0],command);
+    	close(redir_fd);   // close output file if execv fails
+    	exit(1);
+    	
+    }
+    else {
+    	if ( wait(&ch_status) < 0)
+    	{
+    	    return false;
+    	}
+    	if (WEXITSTATUS(ch_status) == EXIT_FAILURE) {
+    		close(redir_fd);
+    		return false;
+    	}
+    	
+    } 
+    close(redir_fd);
 
-
-/*
+/* 
  * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
  *   redirect standard out to a file specified by outputfile.
